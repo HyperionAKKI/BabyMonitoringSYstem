84bec1a3f4a603613e3fddb1f1512dbf4f39324e
AKARSH TIWARI <akarshat2004@gmail.com>
2024-12-13 22:13:38 +0530

    Alerts deaf parents about the baby if it is in another room or not visible
    sensor is calibrated to baby crying frequency the sensor detect baby cries and send a signal through a vibration and led module to alert the deaf parents who cannot hear the cries.

---
+++ BabyMonitoringSystem.ino
@@ -0,0 +1,30 @@
+int soundSensorPin = 2; 
+#include <ezLED.h>
+ezLED led(7);
+void setup() {
+    Serial.begin(9600); 
+    pinMode(soundSensorPin, INPUT);
+    pinMode(4,OUTPUT);
+
+}
+
+void loop() {
+  led.loop();
+    int period = pulseIn(soundSensorPin, HIGH); 
+
+   
+    int frequency = 1000000 / period;
+
+    if (frequency >= 200 && frequency <= 4000) {
+        Serial.print("Frequency (Hz): ");
+        Serial.println(frequency);
+        led.turnON();
+        led.turnOFF(50);
+        digitalWrite(4,1);
+        delay(300);
+        digitalWrite(4,0);
+
+    }
+
+    delay(100); 
+}
